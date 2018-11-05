f834804f59cbd230494150469fd4be179dbba308
maomao <5622876@qq.com>
2018-11-05 14:45:33 +0800

    del

--- apps/zip_helper/README.md
+++
@@ -1,9 +0,0 @@
-zip_helper
-=====
-
-An OTP application
-
-Build
------
-
-    $ rebar3 compile
--- apps/zip_helper/include/state.hrl
+++
@@ -1,5 +0,0 @@
-% -ifdef(DEBUG).
--define(LOG(X), io:format("~n==========log========{~p,~p}==============~n~p~n", [?MODULE,?LINE,X])).
-% -else.
-% -define(LOG(X), true).
-% -endif.
